3e29623c44ca306ac5d8362c4076b10e6732f188
Xuan Hoang <56581733+donghoang2501@users.noreply.github.com>
2019-10-22 16:54:17 +0700

    Delete SDSV.java

--- lab07_Bai2/src/SDSV.java
+++
@@ -1,9 +0,0 @@
-class SDSV {
-    public static void main(String[] args){
-        SinhVien s=new SinhVien("Pham Manh Binh",2000,7.5);
-        s.ghiTep("D:/sv.txt");
-        SinhVien s2=new SinhVien();
-        s2.docTep("D:/sv.txt");
-        s2.hienThi();
-    }
-}
